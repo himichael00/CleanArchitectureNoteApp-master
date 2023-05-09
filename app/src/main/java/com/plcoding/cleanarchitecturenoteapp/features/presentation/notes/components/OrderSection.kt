0dec579bf5d478e33e7b757aaad10fa498510b4a
michael <michaelrio89@gmail.com>
2023-05-09 09:38:02 +0700

    making a radio button and order secction for the UI (not done yet)

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/features/presentation/notes/components/OrderSection.kt
@@ -0,0 +1,35 @@
+package com.plcoding.cleanarchitecturenoteapp.features.presentation.notes.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.plcoding.cleanarchitecturenoteapp.features.domain.util.NoteOrder
+import com.plcoding.cleanarchitecturenoteapp.features.domain.util.OrderType
+
+@Composable
+fun OrderSection (
+    modifier: Modifier = Modifier,
+    noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    onOrderChange: (NoteOrder) -> Unit
+) {
+    Column(
+        modifier = Modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            RadioButton(
+                text = "Title",
+                selected = noteOrder is NoteOrder.Title,
+                onSelect = { onOrderChange(NoteOrder.Title(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            RadioButton(
+                text = "Date",
+                selected = noteOrder is NoteOrder.Date,
+                onSelect = { onOrderChange(NoteOrder.Date(noteOrder.orderType)) }
+            )
+        }
+    }
+}
